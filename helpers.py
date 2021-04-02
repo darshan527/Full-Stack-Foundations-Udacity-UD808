d8c3c717981988287c1eb7fc33739630cdbb9334
darshan527 <darshan.hosamani6@gmail.com>
2021-04-02 23:14:46 +0530

    Helper Methods

--- helpers.py
+++ helpers.py
@@ -9,18 +9,31 @@ session = DBSession()
 
 
 def printDishes():
+    """
+    Prints all the available dishes
+    from the MenuItem Table
+    """
+
     items = session.query(MenuItem).all()
     for item in items:
         print(item.name)
 
 
 def printRestaurant():
+    """
+    Prints all the Restaurant Names
+    from the Restaurant table.
+    """
+
     items = session.query(Restaurant).all()
     for item in items:
         print(item.name)
 
 
 def addRestaurant(resturantName):
+    """
+    Adds a new Restaurant to the database.
+    """
     temp = Restaurant(name=resturantName)
     session.add(temp)
     session.commit()
@@ -29,6 +42,11 @@ def addRestaurant(resturantName):
 def addMenuItem(
     name=None, id=None, course=None, description=None, price=None, restaurant_id=None
 ):
+    """
+    Adds a new Menu Item to the Database,
+    each item is associated with a restaurant.
+    """
+
     temp = MenuItem(
         name=name,
         description=description,
